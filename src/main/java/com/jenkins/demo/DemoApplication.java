f8901b06c3cc56be60d560afc1a655a11d70d6dc
ManikandanSomasundaram <manikandan3sm@gmail.com>
2018-01-18 20:44:16 +0530

    added1

--- src/main/java/com/jenkins/demo/DemoApplication.java
+++ src/main/java/com/jenkins/demo/DemoApplication.java
@@ -8,6 +8,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("<<<--------Jenkins test started ---------->>>");
+		System.out.println("<<<--------Jenkins test started 1---------->>>");
 	}
 }
